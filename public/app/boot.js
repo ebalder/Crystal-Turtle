d116e5b9a7d4d1f97ca4814602726f1fa3b90c9e
Lexas <dislexas@gmail.com>
2014-11-15 21:31:46 -0700

    rewrite routes

--- public/app/boot.js
+++ public/app/boot.js
@@ -1,15 +1,16 @@
-
-
+/*global requirejs, require*/
 
 requirejs.config({
-	baseUrl : '/app/modules',
-	paths : {
-		lib : '/lib',
-		project : '/app/modules/studio/boot',
-		model: '/app/modules/studio/model',
-		studio : '/app/modules/studio',
-		'lib/jquery' : '/lib/jquery-2.0.3.min'
-	},
+    baseUrl : '/app/modules',
+    paths : {
+        lib : '/lib',
+        project : '/app/modules/studio/boot',
+        models: '/app/modules/studio/models',
+        underscore: '/lib/lodash.custom',
+        components: '/app/modules/studio/components',
+        studio : '/app/modules/studio',
+        'lib/jquery' : '/lib/jquery-2.0.3.min'
+    },
     shim: {
         'lib/jquery-ui': {
             export: '$',
@@ -17,40 +18,114 @@ requirejs.config({
         },
     },
     map: { //only absolute paths work here
-    	/* override every call to jquery with jquery-private */
-      '*': { 
-      	'lib/jquery': 'lib/jquery-wrapper',
-      	'lib/peer': 'lib/peer-wrapper'
-      }, 
+        /* override every call to jquery with jquery-wrapper */
+        '*': { 
+            'lib/jquery': 'lib/jquery-wrapper',
+            'lib/peer': 'lib/peer-wrapper'
+        }, 
         /* except for the one in jquery-private */
-      'lib/jquery-wrapper': { 'lib/jquery': 'lib/jquery' },
-      'lib/peer-wrapper': { 'lib/peer': 'lib/peer' }
+        'lib/jquery-wrapper': { 'lib/jquery': 'lib/jquery' },
+        'lib/peer-wrapper': { 'lib/peer': 'lib/peer' },
+        'lib': {
+            backbone: 'lib/backbone'
+        }
     }
 });
 
-requirejs(['lib/jquery', 'lib/jquery-ui', 'navigation'], function($, ui, nav){	
-	function _init(){
-		/* Show or hide buttons from user menu */
-		if(localStorage.sid != null || sessionStorage.sid != null){
-			nav.login();
-		} else {
-			$('a[href="/logout"]').hide();
-			$('a[href="/projectForm"]').hide();
-		}
-		/* navigation events */
-		$('.load').on('click', nav.load);
-		$('.open').on('click', nav.open);
-		$('.popup').on('click', nav.dialog);
-		$('.logout').on('click', nav.logout);
-		/* open hashbang'd path */
-		if(window.location.pathname != '/'){
-			nav.hash(window.location.pathname);
-		}
-	}
-	if(document.readyState == 'complete'){
-		_init();
-	} else {
-		$(document).on('ready', _init());
-	}
+
+requirejs(['lib/jquery','lib/jquery-ui','lib/backbone', /*'session',*/
+    'navigation'], 
+    function($, ui, Backbone, /*session,*/
+        nav)
+{  
+
+    /* add backbone plugins */
+    require(['lib/Backbone.ModelBinder']);
+
+    function _init(){
+        //session.init();
+
+        // TODO: this doesn't handle network timeouts, only various
+        // gateway timeouts.  This all probably should go into a
+        // wrapper class instead that handles common errors, but it
+        // must be reviewed how that interacts with Backbone.
+
+        function retry(xhr, status /*, error */){
+            /* jshint validthis:true */
+            if(this.tryCount < this.maxTries){
+                console.error('%s: %s... retrying', xhr, status);
+                this.tryCount++;
+                var self = this;
+                setTimeout(function(){
+                    $.ajax(self);
+                }, 3000);
+            }
+            else{
+                console.error("couldn't process request!");
+                //ToDo: show some message dialog to the user
+            }
+        }
+
+        /* If network timeout or internal server error, retry */
+        $.ajaxSetup({
+            tryCount: 0,
+            maxTries: 3,
+            statusCode: {
+                408: retry,
+                500: retry,
+                504: retry,
+                522: retry,
+                524: retry,
+                598: retry
+            }
+        });
+    }
+
+    var router;
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "": 'home',
+            "login(/)": 'login',
+            "logout(/)": 'logout',
+            "newProject(/)": 'newProject',
+            "projects(/)": 'browseProjects'
+            "projects/:id(/)": 'project',
+            "signup(/)": 'signup'
+            "users/:id(/)": 'profile',
+        },
+        home: function() {
+            require(['home'], function(view) { view(router); });
+        },
+        login: function(){
+            // require(['login'], function(view) { view(router); });
+        }
+    });
+
+    router = new AppRouter();
+    Backbone.history.start({pushState:true});
+
+    if(document.readyState === 'complete'){
+        _init();
+    } else {
+        document.onready = _init;
+    }
+
+    // if(localStorage.sid != null || sessionStorage.sid != null){
+    //     nav.login();
+    // } else {
+    //     $('a[href="/logout"]').hide();
+    //     $('a[href="/projectForm"]').hide();
+    // }
+    // /* navigation events */
+    // $('.load').on('click', nav.load);
+    // $('.open').on('click', nav.open);
+    // $('.popup').on('click', nav.dialog);
+    // $('.logout').on('click', nav.logout);
+    // /* open hashbang'd path */
+    // if(window.location.pathname != '/'){
+    //     nav.hash(window.location.pathname);
+    // }
+
+    return;
 });
 
